93cefd33d94dc7f2e0d9381467fda11e570690d0
hasancerit <hasan__cerit@hotmail.com>
2019-05-09 19:54:19 +0300

    second

--- src/attrp/RequestController.java
+++ src/attrp/RequestController.java
@@ -27,9 +27,19 @@ public class RequestController extends HttpServlet{
 		kisiler.add(kisi2);
 		kisiler.add(kisi3);
 		
-		req.setAttribute("KisiList",kisiler);
+		req.setAttribute("KisiList",kisiler);//Kisileri request scope alanýnda attribute'e atadýk
 		
+		/*Dispatcher, yönlendiricidir. Redirect'ten farklar;
+		 *1-forward metodunda url degismez , sendRedirect metodunda url degisir.
+		 *2-Dispatch islemi  server’da olur , Redirect islemi ise client’ta olur.
+		 *3-Redirect islemini soyle dusunebiliriz ; musteri bir problemle karsilasti ve sizi aradi siz bu isten ben sorumlu 
+		 *degilim diyerek musteriyi farkli bir kisiye yonlendiriyorsunuz. (url degisir)
+		 *4-Dispatch islemi ise ; musterinin karsilastigi problemi cozerken calisma arkadasinizdan yardim aliyorsunuz arka 
+		 *planda isin sizin yaptiginiz sanilirken aslinda calisma arkadasiniz bu isi yapmaktadir. 
+		 *Musterinin bundan haberi yoktur.( url degismez)*/
 		RequestDispatcher dispatcher = req.getRequestDispatcher("JSPs/firstJsp.jsp");
-		dispatcher.forward(req, resp);
+		//Dispatcherý reqten çekersek, Relative Path ile dosyalara ulaþabiliriz.
+		//Dispatcherý Servlet Contextten çekersel Absolute path ile dosyalara ulaþabiliriz.
+		dispatcher.forward(req, resp);//Yönlendirme yapýldý.
 	}
 }
